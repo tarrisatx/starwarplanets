af8958704cdc1cd4342a96d043ef2e5ce312e098
Tahara Ezell <taharaezell@webheadtech.com>
2019-02-03 00:36:46 -0600

    Updates

---
+++ precache-manifest.1420c828110b7b93977f4afd850c283f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/starwarplanets/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ddea5480c87acbb88fea",
+    "url": "/starwarplanets/static/js/runtime~main.ddea5480.js"
+  },
+  {
+    "revision": "dde1d098fc37ce0593ad",
+    "url": "/starwarplanets/static/js/main.dde1d098.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/starwarplanets/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dde1d098fc37ce0593ad",
+    "url": "/starwarplanets/static/css/main.2ba6454e.chunk.css"
+  },
+  {
+    "revision": "aaafee3772b1dcafaae07aa2a1401c17",
+    "url": "/starwarplanets/index.html"
+  }
+];
